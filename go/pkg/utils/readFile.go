7973d0767f1ac059b89cee02718746b6353917ff
Daniel De Pinto <daniel.depinto@gmail.com>
2022-01-12 20:48:04 -0800

    debugging readFile

--- go/pkg/utils/readFile.go
+++ go/pkg/utils/readFile.go
@@ -1,14 +1,20 @@
 package utils
 
 import (
-	//"fmt"
+	"fmt"
 	"bufio"
 	"path/filepath"
 	"os"
 )
 
 func NewStringSliceFromFileName(fileName string) ([]string, error) {
-	filePath := filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "DePinto", "aoc-2021", "input", fileName)
+	filePath := filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "DePinto", "aoc-2021", "input", fileName) // works local, fails github
+	//filePath, err := filepath.Abs("")
+	
+	fmt.Printf("filepath:\n%v\n\n", filePath)
+	// if err != nil {
+	// 	panic(err)
+	// }
 	
 	file, err := os.Open(filePath)
 	if err != nil {
